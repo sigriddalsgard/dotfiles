7e7bbddecbe5c32f531384a2a092b2afe1419892
Sigrid Gyldenkaerne Dalsgard <sigrid.dalsgard@gmail.com>
2015-04-30 14:04:36 +0200

    Commit stuff.

--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,7 @@
 # Aliases shared between fish and bash
 . "$HOME/.aliases"
+
+# Set Java classpath
+set -x CLASSPATH ".:$HOME/Development/java/lib/*"
+
+set -x GOPATH "$HOME/Go"; set PATH $GOPATH/bin $PATH
